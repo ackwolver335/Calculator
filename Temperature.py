270dfcd4f9b638d63cfbb53f87e774823168dec6
ackwolver335 <abhaych335@gmail.com>
2024-07-14 15:58:33 +0530

    Celsius Class updated in Temperature Module

--- Temperature.py
+++ Temperature.py
@@ -2,26 +2,121 @@
 # Following units are envolved here
 # Celsius, Fahrenheit, Kelvin, Rankine, reaumur
 
+# It contains different methods regariding the calculation of temperature in different available units
+# further description is been given below regarding the code and method's explanation
+
+"""---- Main File contains the Methods or Functions that are mentioned below ! -----"""
+
+celsius_methods = [
+    '__init__(self)',
+    'c2F(self,value)',
+    'c2k(self,value)',
+    'c2rrankine(self,value)',
+    'c2reaumur'
+]
+
 class celsius:
     celsius = 0
 
-    def __init__(self,C):
-        self.celsius = C
+    # ------------ Initial Methods for initiating objects and their overview on different available methods ------------------
+    # ------------------------------------------------------------------------------------------------------------------------
+    # This Module together with the intial one have its overall proper data over to the Github : @ackwolver335 ---------------
 
-    def c2F(self):
-        celsius = (self.celsius * 1.8) + 32
+    def __init__(self):
+        """
+            :: Temperature Module ::
+
+            Class : celsius
+            Function : __init__(self) it don't contains different arguments at its initial phase
+            We are getting the data at the runtime of the Program.
+
+            Also the available data members of this one is explained below :
+            1. celsius : This is generally used in order to store the value after getting converted in the required unit in Temperature.
+
+            Temperature Module -> Designed by Abhay Chaudhary.
+        """
+
+        # Here we have a general use of pass keyword as we don't have further code in __init__() method.
+        pass
+
+    def c2F(self,value):
+
+        """
+            :: Temperature Module ::
+
+            Class : celsius
+            Function : c2F(self,value) in general for conversion of one unit into another, we uses this value.
+
+            Arguments :
+            value : It stands for the value of celsius to be converted into Fahrenheit Temperature Unit.
+
+            Temperature Module -> Designed by Abhay Chaudhary
+        """
+
+        # main source code for conversion of units
+        # contains the formula for conversion of celsius into Fahrenheit Unit
+
+        celsius = (value * 1.8) + 32
         return celsius
 
-    def c2k(self):
-        celsius = self.celsius + 273.15
+    def c2k(self,value):
+
+        """
+            :: Temperature Module ::
+
+            Class : celsius
+            Function : c2k(self,value) in general for conversion of celsius into kelvin temperature with the use of this value.
+
+            Arguments :
+            1. value : It is the main source value regarding the conversion.
+
+            Temperature Module -> Designed by Abhay Chaudhary.
+        """
+
+        # main source code for conversion of units
+        # contains the formula for conversion of celsius into kelvin unit
+
+        celsius = value + 273.15
         return celsius
 
-    def c2rrankine(self):
-        celsius = ((self.celsius * 1.8) + 32) + 459.67
+    def c2rrankine(self,value):
+
+        """
+            :: Temperature Module ::
+
+            Class : celsius
+            Function : c2rrankine(self,value), it contains value argument and self for conversion of temperature into rankine unit.
+
+            Arguments :
+            1. value : It is the value of celsius used for conversion into kelvin unit.
+
+            Temperature Module -> Designed by Abhay Chaudhary.
+        """
+
+        # main source code for conversion of units
+        # contains the formula for conversion of celsius unit into rankine unit
+
+        celsius = ((value * 1.8) + 32) + 459.67
         return celsius
 
-    def c2reaumur(self):
-        celsius = self.celsius * 0.8
+    def c2reaumur(self,value):
+
+        """
+            :: Temperature Module ::
+
+            Class : celsius
+            Function : c2reaumur(self,value) its main purpose is to convert the celsius temperature unit into reaumur unit.
+
+            Arguments :
+            1. value : It is the value of celsius used for conversion into reaumur units.
+
+            Temperature Module -> Designed by Abhay Chaudhary.
+        """
+
+        # main source code for conversion of units
+        # contains the formula for conversion of celsius unit into reaumur unit
+
+        celsius = value * 0.8
         return celsius
 
 class Fahrenheit:
