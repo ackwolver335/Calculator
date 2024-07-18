a7feeed6d00a3fa37767aeafb80c4c71b4315da0
ackwolver335 <abhaych335@gmail.com>
2024-07-18 12:57:16 +0530

    Discount Method Code Update

--- Discount.py
+++ Discount.py
@@ -1,15 +1,21 @@
 # Here is another Module in Calculator Library created using Python which will provide the outcomes of Discount
 # Following is the class of this one 
 
+"""------------------------------------------ Discount Module  ---------------------------------------------"""
+
+methods = [
+    '__init__(self)',
+    'final_price(self,orgprice,discount)',
+]
+
 class Discount:
     original_price = 0
     discount_percent = 0
 
-    def __init__(self,orgprice,discount):
-        self.original_price = orgprice
-        self.discount_percent = discount
+    def __init__(self):
+        pass
 
-    def final_price(self):
-        discount_price = (self.discount_percent / 100) * self.original_price
-        final_price = self.original_price - discount_price
+    def final_price(self,org_price,discount):
+        discount_price = (discount / 100) * org_price
+        final_price = org_price - discount_price
         return final_price
